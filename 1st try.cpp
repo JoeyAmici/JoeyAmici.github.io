c81c0fb9cdb3f03f1e281b3289024a16c94788b8
JoeyAmici <112204562+JoeyAmici@users.noreply.github.com>
2022-12-15 14:25:15 -0800

    Joey Amici's Portfolio
    This is a collection of the projects I have worked on throughout my career as a Computer Engineer

---
+++ 1st try.cpp
@@ -0,0 +1,34 @@
+# Joey Amicis portfolio
+
+Hello, My name is Joey Amici and I am a full-time student at
+[California State College Fullerton](http://fullerton.edu).
+My major is Computer Engineering and I want to join the USAF
+after graduating and go for my Masters in Computer Engineering.
+
+## My favorite projects
+With my first semester over at [California State College Fullerton](http://fullerton.edu),
+this is a collection of my favorite projects thus far.
+
+### [Lab 07 Part 02](https://github.com/cpsc-pilot-fall-2022/cpsc-120-lab-07-joey-julian/tree/main/part-2)
+
+This lab was one of my favorite labs because me and my partner Julian
+had a hard time coding the Ace to be two different values that change depending on other factors.
+It was my first time coding a variable that can change depending on other factors in the code.
+
+### [Lab 03 All Parts](https://github.com/cpsc-pilot-fall-2022/cpsc-120-lab-03-JoeyAmici)
+
+Lab 03 was the first time using [Github](http://github.com) and my first time coding in [C++](https://en.wikipedia.org/wiki/C%2B%2B).
+I had come into this class knowing how to code in Java. I thought it would be easy to do this lab, but I was wrong. It humbled me and made
+me realize that using [Github](http://github.com) was going to be difficult. As it got later in the semester, I found myself loving [Github](http://github.com).
+It made it so easy to code remotly from home and keep all my code in repositories that I could access from any computer. I do not think I can go back to the way
+I used to code before I knew about [Github](http://github.com).
+
+### [Lab 12 All Parts](https://github.com/cpsc-pilot-fall-2022/cpsc-120-lab-12-joey)
+
+Lab 12 was THE hardest lab I had done so far in CPSC 120L. The code was overwelming. It took me hours upon hours to figure it out at home.
+I was finally able to figure it out when I asked what was wrong with my code.
+
+### What I will do with this page
+
+This page will be updated even after I leave CPSC 120L. I want to build this portfolio so when people ask for projects I have done in the past.
+I can send them this page.
